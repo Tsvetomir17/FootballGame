b8faa902e5c59ce7768e4e8184d1f53dd5e9fa95
Tsvetomir <ts_mladenov@abv.bg>
2023-03-09 01:04:24 +0200

    Make the fillTheListOfFootballPlayers() method in different class

---
+++ src/Game/GetAllPlayersFromDB.java
@@ -0,0 +1,38 @@
+package Game;
+
+import FootballPlayer.FootballPlayer;
+
+import java.sql.*;
+
+import static Game.Game.theFullDeckOfFootballPlayers;
+
+public class GetAllPlayersFromDB {
+    public static void fillTheListOfFootballPlayers() throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/FootballGamePlayers";
+        String username = "postgres";
+        String password = "password";
+
+        Connection connection = DriverManager.getConnection(url,username,password);
+        String query = "SELECT * FROM \"FootballPlayers\" ORDER BY RANDOM()";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+
+        while(resultSet.next()){
+
+            FootballPlayer footballPlayerToPushInTheDeck = new FootballPlayer(
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getInt(4),
+                    resultSet.getString(5),
+                    resultSet.getBoolean(6),
+                    resultSet.getBoolean(7),
+                    resultSet.getBoolean(8),
+                    resultSet.getInt(9),
+                    resultSet.getInt(10)
+            );
+            theFullDeckOfFootballPlayers.push(footballPlayerToPushInTheDeck);
+        }
+    }
+}
