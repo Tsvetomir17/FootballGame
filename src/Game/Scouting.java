03929abe39a17004f45b25446d748a28f1dd1f55
Tsvetomir <ts_mladenov@abv.bg>
2023-03-09 01:03:18 +0200

    Make class for the scouting part of the season

---
+++ src/Game/Scouting.java
@@ -0,0 +1,63 @@
+package Game;
+
+import FootballPlayer.FootballPlayer;
+import Player.Player;
+
+import static Game.Game.*;
+import static GameModes.FinalVariables.MAX_FOOTBALL_PLAYERS_IN_TEAM_SIZE;
+
+public class Scouting {
+
+    public static void scouting(){
+        for (int i = 0; i < getPlayersInTheGameSize(); i++) {
+            Player currentPlayer = players.get(teamColoursInCurrentOrder.get(i));
+
+            int playersThatCanBeScoutedSize = currentPlayer.getPlayerUpgradeBoard().getScoutingStaffLevel();
+            System.out.println(currentPlayer.getPlayerColour() +", it is your turn to do scouting!");
+            System.out.println("You can scout "+ playersThatCanBeScoutedSize + " players.");
+
+            while(playersThatCanBeScoutedSize > 0){
+                System.out.println("You have " + currentPlayer.getPlayerMoney() + "M.");
+                playersThatCanBeScoutedSize--;
+                if (!validateIfThePlayerCanAddFootballPlayer(currentPlayer)){
+                    System.out.println("Your team is full, you can not add more players right now");
+                    break;
+                }
+
+                FootballPlayer scoutedPlayer = theFullDeckOfFootballPlayers.pop();
+                scoutedPlayer.printFootballPlayerAsCard();
+
+                if(!checkIfThePlayerHasEnoughMoney(currentPlayer,scoutedPlayer)) continue;
+
+                if(choiceMadeByUser() == 1){
+                    buyPlayer(currentPlayer,scoutedPlayer);
+                }
+            }
+        }
+    }
+
+    private static boolean validateIfThePlayerCanAddFootballPlayer(Player player){
+        return player.getFullTeam().getFullTeamSize() < MAX_FOOTBALL_PLAYERS_IN_TEAM_SIZE;
+    }
+
+    private static int choiceMadeByUser(){
+        System.out.println("1. Buy");
+        System.out.println("2. Skip");
+
+        return choiceMadeByTheUserValidation(0,2);
+    }
+
+    private static void buyPlayer(Player currentPlayer, FootballPlayer scoutedPlayer){
+        currentPlayer.addFootballPlayerToTheTeam(scoutedPlayer);
+        currentPlayer.spendMoney(scoutedPlayer.getScoutingPrice());
+        System.out.println("You successfully bought " + scoutedPlayer.getFootballPlayerName() + "!");
+    }
+
+    private static boolean checkIfThePlayerHasEnoughMoney(Player currentPlayer, FootballPlayer scoutedPlayer){
+        if(currentPlayer.getPlayerMoney() < scoutedPlayer.getScoutingPrice()){
+            System.out.println("You do not have enough money for this player");
+            return false;
+        }
+        return true;
+    }
+}
