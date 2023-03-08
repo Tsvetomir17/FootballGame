191f9705279d68148931643d75f7a656ac4054ad
Tsvetomir <ts_mladenov@abv.bg>
2023-03-09 01:05:29 +0200

    Make class for the draft part of the game

---
+++ src/Game/Draft.java
@@ -0,0 +1,50 @@
+package Game;
+
+import FootballPlayer.FootballPlayer;
+import Player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static Game.Game.*;
+import static GameModes.FinalVariables.DRAFT_SIZE;
+
+public class Draft {
+
+    public static void setTheFootballPlayersDraft(){
+        List<FootballPlayer> currentPlayersInTheDraft = new ArrayList<>();
+        int currentIndexForTheTeamToPick = 0;
+        Player currentPlayerToPick;
+        for (int i = 0; i < getPlayersInTheGameSize(); i++) {
+
+            currentPlayersInTheDraft = setPlayersToBeDrafted();
+
+            while (!currentPlayersInTheDraft.isEmpty()) {
+
+                printFootballPlayersAsCards(currentPlayersInTheDraft);
+
+                currentPlayerToPick = players.get(teamColoursInCurrentOrder.get(currentIndexForTheTeamToPick));
+                System.out.println(currentPlayerToPick.getPlayerColour() + ", it is your turn to pick: ");
+                int choiceByPlayer = choiceMadeByTheUserValidation(0, currentPlayersInTheDraft.size()) -1;
+
+                currentPlayerToPick.addFootballPlayerToTheTeam(currentPlayersInTheDraft.get(choiceByPlayer));
+
+                currentPlayersInTheDraft.remove(choiceByPlayer);
+                currentIndexForTheTeamToPick = setCurrentIndexForTeamToPick(currentIndexForTheTeamToPick);
+            }
+        }
+    }
+
+    private static List<FootballPlayer> setPlayersToBeDrafted(){
+        List<FootballPlayer> currentPlayersInTheDraft = new ArrayList<>();
+        for (int j = 0; j < DRAFT_SIZE; j++) {
+            currentPlayersInTheDraft.add(theFullDeckOfFootballPlayers.pop());
+        }
+
+        return currentPlayersInTheDraft;
+    }
+
+    private static int setCurrentIndexForTeamToPick(int index){
+        return ++index == getPlayersInTheGameSize() ? 0 : index;
+    }
+}
