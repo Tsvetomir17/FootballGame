785345563116aedc20cc51ef4756c0ff7daf4d4c
Tsvetomir <ts_mladenov@abv.bg>
2023-03-09 20:37:38 +0200

    class for discarding part of the season

---
+++ src/Game/Discarding.java
@@ -0,0 +1,45 @@
+package Game;
+
+import Player.Player;
+
+import static Game.Game.*;
+
+public class Discarding {
+
+    public static void discarding(){
+        for (int i = 0; i < getPlayersInTheGameSize(); i++) {
+
+            Player currentPlayer = players.get(teamColoursInCurrentOrder.get(i));
+            printStartingMessage(currentPlayer);
+            int choiceByPlayer = choiceMadeByTheUserValidation(-1,currentPlayer.getFullTeam().getFullTeamSize());
+            while (choiceByPlayer != 0){
+                removeFootballPlayerAtIndex(choiceByPlayer, currentPlayer);
+                printStartingMessage(currentPlayer);
+                choiceByPlayer = choiceMadeByTheUserValidation(-1,currentPlayer.getFullTeam().getFullTeamSize());
+            }
+        }
+    }
+
+    private static void printStartingMessage(Player currentPlayer){
+        System.out.println(currentPlayer.getPlayerColour() + ", it is your turn to discard.");
+        System.out.println("You can choose a player to be discarded or press '0' to skip this phase");
+        System.out.println("0. Skip");
+        currentPlayer.printFullTeam();
+        System.out.println("Choose a player, if you want to discard him: ");
+
+    }
+
+    private static void removeFootballPlayerAtIndex(int index, Player player){
+        if(index < 0 || index > player.getFullTeam().getFullTeamSize()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if(player.getFullTeam().getFullTeamSize() <= 11){
+            System.out.println("You can not discard more players, because you cant have less than 11 players");
+            return;
+        }
+
+        player.addMoneyToThePlayer(player.getFullTeam().getFootballPlayerAtIndex(index-1).getScoutingPrice());
+        player.discardFootballPlayerFromTheTeam(index-1);
+    }
+}
