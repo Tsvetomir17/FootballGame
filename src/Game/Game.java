41860a92ba3a00236b546075f778e380d2a5f391
Tsvetomir <ts_mladenov@abv.bg>
2023-03-15 17:38:20 +0200

    add season and afterseason

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,6 +6,7 @@ import Player.Player;
 import java.sql.*;
 import java.util.*;
 
+import static Game.AfterSeason.afterSeason;
 import static Game.DeadlineDay.deadlineDay;
 import static Game.Discarding.discarding;
 import static Game.Draft.setTheFootballPlayersDraft;
@@ -15,6 +16,7 @@ import static Game.Scouting.scouting;
 import static Game.SetPlayersColoursAndMoney.*;
 import static Game.PlayerDevelopment.upgradeFootballPlayers;
 import static Game.Upgrades.upgrades;
+import static Season.Season.season;
 
 public class Game {
     private static int playersInTheGameSize;
@@ -24,7 +26,7 @@ public class Game {
     private static final Game instance = new Game();
 
     private Game(){
-        players = new HashMap<String, Player>();
+        players = new HashMap<>();
         teamColoursInCurrentOrder = new ArrayList<>();
         theFullDeckOfFootballPlayers = new Stack<>();
     }
@@ -33,11 +35,16 @@ public class Game {
         return instance;
     }
 
-    private void setPlayersInTheGameSize(){
+    private static void setPlayersInTheGameSize(){
         playersInTheGameSize = choiceMadeByTheUserValidation(2,6);
     }
 
-    public void startGame() throws SQLException, ClassNotFoundException {
+    public void startGame() throws SQLException, ClassNotFoundException, InterruptedException {
+        firstSeason();
+        nextSeason();
+    }
+
+    private static void firstSeason() throws SQLException, ClassNotFoundException, InterruptedException {
         printStartingMessage();
         setPlayersInTheGameSize();
         playersChoosingTeamColours();
@@ -45,24 +52,28 @@ public class Game {
         fillTheListOfFootballPlayers();
         setTheFootballPlayersDraft();
         upgradeFootballPlayers();
-        discarding();
         scouting();
         upgrades();
         deadlineDay();
         setLineUp();
+        season(players,teamColoursInCurrentOrder);
+    }
+    private static void nextSeason() throws SQLException, ClassNotFoundException, InterruptedException {
+        while(afterSeason()){
+            upgradeFootballPlayers();
+            discarding();
+            scouting();
+            upgrades();
+            deadlineDay();
+            setLineUp();
+            season(players,teamColoursInCurrentOrder);
+        }
     }
 
     public static void printFootballPlayers(List<FootballPlayer> players){
         for (int i = 0; i < players.size(); i++) {
             System.out.print((i+1) + ". ");
             players.get(i).printFootballPlayer();
-        }
-    }
-
-    public static void printFootballPlayersAsCards(List<FootballPlayer> players){
-        for (int i = 0; i < players.size(); i++) {
-            System.out.println((i+1) + ".");
-            players.get(i).printFootballPlayerAsCard();
         }
     }
 
