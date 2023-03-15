8598c9a78883a1977e4fb1b0720aafc9b566bb08
Tsvetomir <ts_mladenov@abv.bg>
2023-03-15 17:35:48 +0200

    add season class

--- src/Season/Season.java
+++ src/Season/Season.java
@@ -1,12 +1,33 @@
 package Season;
 
+import Player.Player;
+
+import java.util.List;
+import java.util.Map;
+
+import static Season.GameModes.*;
+import static Season.PrintFunctionsForSeason.printStartOfTheSeasonMessage;
+
 public class Season {
 
-    public static void season(int playersSize){
+    public static void season(Map<String, Player> players, List<String> teamColoursInCurrentOrder) throws InterruptedException {
+        printStartOfTheSeasonMessage();
+        setALlPlayersCurrentPointsAsTheirOverall(players);
 
-        switch (playersSize){
-            case 2 -> System.out.println(2);
-            case 3 -> System.out.println(3);
+        switch (players.size()){
+            case 2 -> gameModeForTwoPlayers(players,teamColoursInCurrentOrder);
+            case 3 -> gameModeForThreePlayers(players,teamColoursInCurrentOrder);
+            case 4 -> gameModeForFourPlayers(players,teamColoursInCurrentOrder);
+            case 5 -> gameModeForFivePlayers(players,teamColoursInCurrentOrder);
+            case 6 -> gameModeForSixPlayers(players,teamColoursInCurrentOrder);
         }
     }
+
+    private static void setALlPlayersCurrentPointsAsTheirOverall(Map<String, Player> players){
+        for (Map.Entry<String, Player> player : players.entrySet()){
+            player.getValue().setCurrentPointsInTheSeason(player.getValue().getCurrentPlayerOverall());
+        }
+    }
+
+
 }
